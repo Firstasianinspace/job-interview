0cc2b40e9e644ce6cbbde04f5381d6e2750e3063
Aleksandr Rabdaev <firstasianinspace@gmail.com>
2024-03-26 02:29:23 +0700

    sleepy joe

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@
     <strong>Current route path:</strong> {{ $route.fullPath }}
   </p>
   <nav>
-    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/">Go to First</RouterLink>
+    <RouterLink to="/About">Go to Second</RouterLink>
   </nav>
   <main>
     <RouterView />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { reactive, computed, watch, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 
-
-// @TODO to files
 interface ISport {
   id: number;
   name: string;
@@ -16,42 +14,53 @@ const sports: ISport[] = [
   { id: 2, name: "Basketball" },
   { id: 3, name: "Volleyball" },
 ];
-
-const filters = ref({
+// reactive more suitable/scalable for rich-object filters
+const filters = reactive({
   sport: null as number | null,
 });
 
+const selectedSportName = computed(() => sports.find(sport => sport.id === filters.sport)?.name || '');
+
 const updateQuery = () => {
   const query: Record<string, string> = {};
-  console.log(filters.value.sport !== null)
-  if (filters.value.sport !== null) {
-    query['sportId'] = filters.value.sport.toString();
-    console.log(filters.value.sport)
-    query['sportName'] = sports.find(sport => sport.id === filters.value.sport)?.name || '';
+  if (filters.sport !== null) {
+    query['sportId'] = filters.sport.toString();
+    query['sportName'] = selectedSportName.value;
   }
-  router.push({query});
-}
+  router.push({ query });
+};
 
 const initializeFiltersFromQuery = () => {
-  console.log(router)
   const query = router.currentRoute.value.query;
   if (query.sportId) {
-    filters.value.sport = parseInt(query.sportId as string);
+    filters.sport = parseInt(query.sportId as string);
   }
 }
 
 const resetFilter = () => {
-  filters.value.sport = null;
+  filters.sport = null;
   updateQuery();
-}
+};
+
+watch(
+  () => router.currentRoute.value.query,
+  (newQuery) => {
+    if (newQuery.sportId) {
+      filters.sport = parseInt(newQuery.sportId as string);
+    } else {
+      filters.sport = null;
+    }
+  },
+  { deep: true }
+);
+
 
 onMounted(() => {
-  initializeFiltersFromQuery();
+  initializeFiltersFromQuery()
 });
 </script>
 <template>
   <div class="app-container">
-    {{ filters }}
     <select v-model="filters.sport" @change="updateQuery">
       <option v-for="sport in sports" :key="sport.id" :value="sport.id">
         {{ sport.name }}
